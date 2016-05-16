c6b2cda58a72abce8bec0fdaf2dd017d62c4a986
Juan Lagostena <jlagostena@bitsense.com.ar>
2016-05-16 01:32:09 -0300

    Agregadas algunas otras situaciones al script

--- http_cache_cache_control.php
+++ http_cache_cache_control.php
@@ -1,8 +1,12 @@
 <?php
 
-header("Cache-Control: max-age=180");
-
 $expiresString = gmdate('D, d M Y H:i:s', time() + (180)) . ' GMT';
+
+#header("Cache-Control: max-age=180");
+header("Cache-Control: no-cache");
+header("Expires: ".$expiresString);
+
+
 echo "FECHA: ".date('Y m d H:i:s');
 #echo date('Y m d H:i:s');
 
